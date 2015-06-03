8698d8fa13b42621f366bcd568cee55a00334889
albertosouza <alberto.souza.99@gmail.com>
2015-06-03 03:31:42 -0300

    update polymer app and plugins

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,51 +1,36 @@
-/*
-Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-
 (function (document) {
   'use strict';
 
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
-
-  /**
-   * Add Accept in all request
-   *
-   */
-  $.ajaxPrefilter(function( options ) {
-    if ( !options.beforeSend) {
-      options.beforeSend = function (xhr) {
-        xhr.setRequestHeader('Accept', 'application/json');
-        // set auth token
-        if (app.auth && app.auth.token)
-          xhr.setRequestHeader('Authorization','Bearer ' + app.auth.token);
-      };
-    }
-  });
-
-  app.displayInstalledToast = function() {
-    document.querySelector('#caching-complete').show();
-  };
-
   // See https://github.com/Polymer/polymer/issues/1381
   window.addEventListener('WebComponentsReady', function() {
-    // document.querySelector('body').removeAttribute('unresolved');
+    document.querySelector('body').removeAttribute('unresolved');
+    document.querySelector('noscript').remove();
+    // Grab a reference to our auto-binding template
+    // and give it some initial binding values
+    // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+    var app = document.querySelector('#app');
 
-    var drawerPanel = document.querySelector('#paperDrawerPanel');
+    /**
+     * Add Accept header in all request
+     */
+    $.ajaxPrefilter(function( options ) {
+      if ( !options.beforeSend) {
+        options.beforeSend = function (xhr) {
+          xhr.setRequestHeader('Accept', 'application/json');
+          // set auth token
+          if (app.auth && app.auth.token)
+            xhr.setRequestHeader('Authorization','Bearer ' + app.auth.token);
+        };
+      }
+    });
+
+    app.displayInstalledToast = function() {
+      document.querySelector('#caching-complete').show();
+    };
+
+    var loadMenu = $.get('/api/v1/docs/we/menu').then(function(r) {
+      app.docMenu = r.menu;
+    });
   });
 
 })(document);
-
-// TODO: Decide if we still want to suggest wrapping as it requires
-// using webcomponents.min.js.
-// wrap document so it plays nice with other libraries
-// http://www.polymer-project.org/platform/shadow-dom.html#wrappers
-// )(wrap(document));
-//
