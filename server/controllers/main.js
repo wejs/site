0d5357b4b1a378135f7289a7a364c8af1bf15864
albertosouza <alberto.souza.99@gmail.com>
2015-09-02 03:55:50 -0300

    remove layoutName config from controller

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -11,9 +11,6 @@ module.exports = {
 
     res.locals.template = 'home/index';
 
-    // use fullwidth layout in home page
-    res.locals.layoutName = 'fullwidth';
-
     res.view({ title: we.config.appName });
   }
 };
