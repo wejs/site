509bf662403ed83d1f779dc410d58777b479339a
albertosouza <alberto.souza.99@gmail.com>
2015-09-23 06:16:01 -0300

    update configs

--- config/github.js
+++ config/github.js
@@ -3,6 +3,6 @@ module.exports.github = {
   debug: true, // console.log,
   authentication: { //via token (Personal access token)
     type: "token",
-    token: "f191d9ee659a92068e0aa8d1880a5abecd1c6802" // read only token
+    token: "" // read only token
   }
 };
