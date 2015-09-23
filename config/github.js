99beb617a1047202a0dee7b3ff0e926d56b92398
albertosouza <alberto.souza.99@gmail.com>
2015-09-23 05:39:03 -0300

    add read only github config

---
+++ config/github.js
@@ -0,0 +1,8 @@
+module.exports.github = {
+  version: "3.0.0", // API version
+  debug: true, // console.log,
+  authentication: { //via token (Personal access token)
+    type: "token",
+    token: "f191d9ee659a92068e0aa8d1880a5abecd1c6802" // read only token
+  }
+};
