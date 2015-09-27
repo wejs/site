86dd518d70dbae7cd26504c29c3785bbf9569875
albertosouza <alberto.souza.99@gmail.com>
2015-09-27 00:16:35 -0300

    add home-community widget

---
+++ server/widgets/home-community/index.js
@@ -0,0 +1,10 @@
+/**
+ * Widget home-community main file
+ *
+ * See https://github.com/wejs/we-core/blob/master/lib/class/Widget.js for all Widget prototype functions
+ */
+
+module.exports = function homeCommunityWidget(projectPath, Widget) {
+  var widget = new Widget('home-community', __dirname);
+  return widget;
+};
