149770befce2c860425084f8e283a8e8a7888065
albertosouza <alberto.souza.99@gmail.com>
2015-10-04 20:51:56 -0300

    remove uneed github config

--- config/github.js
+++
@@ -1,8 +0,0 @@
-module.exports.github = {
-  version: "3.0.0", // API version
-  debug: true, // console.log,
-  authentication: { //via token (Personal access token)
-    type: "token",
-    token: "" // read only token
-  }
-};
