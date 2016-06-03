d75e7c86cbaa113c4918896cae3d7b368e2037e4
albertosouza <alberto.souza.99@gmail.com>
2016-06-02 23:44:10 -0300

    update build and npm modules

--- files/public/build/prod.concat.footer.js
+++ files/public/build/prod.concat.footer.js
@@ -53082,25 +53082,6 @@ window.we.components.editor = {
     element.summernote(cfg);
   }
 };
-/**
- * Add Accept in all requests by default
- */
-$.ajaxPrefilter(function( options ) {
-  if ( !options.beforeSend) {
-    options.beforeSend = function (xhr) {
-      xhr.setRequestHeader('Accept', 'application/json');
-
-      // set auth token
-      if ($.cookie('weoauth')) xhr.setRequestHeader('Authorization','Bearer ' + $.cookie('weoauth'));
-    };
-  }
-});
-
-(function (we) {
-
-  we.oauth2 = {};
-
-})(window.we);
 (function (we) {
 
   if (we.config.widgetContext) {
